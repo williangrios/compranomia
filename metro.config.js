6b0acbeceed7818aad0a03e9776d49dfb6df642d
Willian Rios <williangrios@yahoo.com.br>
2026-04-07 08:27:39 -0300

    chore: change config

--- metro.config.js
+++ metro.config.js
@@ -6,4 +6,9 @@ const config = getDefaultConfig(__dirname)
 
 config.resolver.assetExts = [...(config.resolver.assetExts || []), 'ttf']
 
+// Ignorar arquivos temporários do node_modules/.bin no Windows
+config.resolver.blockList = [
+  /node_modules[/\\]\.bin[/\\]\..*/,
+]
+
 module.exports = config
